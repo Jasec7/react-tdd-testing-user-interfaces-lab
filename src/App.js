d09247bfe07dc8443d970cde1eec318087a7682c
Jasec <jasec_7@hotmail.com>
2025-06-19 02:19:06 -0700

    complete react tdd user interfaces lab

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 function App() {
-  return <div>Your code here</div>;
+  return <div>
+    <h1>Hi, I'm Jasec</h1>
+    <img src="https://superstarsbio.com/wp-content/uploads/2019/08/Chris-Hemsworth.webp" alt="Jasec blonde"
+    />
+    <h2>About Me</h2>
+    <p>Learning TDD</p>
+     <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github
+        </a>
+         <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
+        </a>
+  </div>;
 }
 
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,70 @@
 import { render, screen } from "@testing-library/react";
 import '@testing-library/jest-dom';
-
 import App from "../App";
 
 // Your tests here
+test("displays a top-level heading with the text `Hi, I'm _______`", () => {
+  // Arrange
+  render(<App/>);
+  // Act
+  const topLevelHeading = screen.getByRole("heading", {
+    name: /hi, i'm/i,
+    exact: false,
+    level: 1,
+  });
+   // Assert
+  expect(topLevelHeading).toBeInTheDocument();
+});
+
+
+test("displays an image with alt identifying the content of the image", () => {
+  // Arrange
+  render(<App/>);
+  // Act
+  const imageContent = screen.getByAltText(/Jasec blonde/i);
+   // Assert
+  expect(imageContent).toBeInTheDocument();
+});
+
+
+test("displays a second level heading with the text `About Me`", () => {
+  // Arrange
+  render(<App/>);
+  // Act
+  const secondLevelHeading = screen.getByText(/About Me/i,);
+   // Assert
+  expect(secondLevelHeading).toBeInTheDocument();
+});
+
+
+test("displays a paragraph in the biography", () => {
+  // Arrange
+  render(<App/>);
+  // Act
+  const paragraph = screen.getByText(/Learning TDD/i,);
+   // Assert
+  expect(paragraph).toBeInTheDocument();
+});
+
+
+test("displays a link for Github", () => {
+  // Arrange
+  render(<App/>);
+  // Act
+  const gitLink = screen.getByRole("link",{name:/Github/i});
+   // Assert
+  expect(gitLink).toBeInTheDocument();
+});
+
+
+test("displays a link for LinkedIn", () => {
+  // Arrange
+  render(<App/>);
+  // Act
+  const linkedLink = screen.getByRole("link",{name:/LinkedIn/i});
+   // Assert
+  expect(linkedLink).toBeInTheDocument();
+});
+
+
+
